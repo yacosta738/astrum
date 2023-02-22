0f704b8c8e99de3985d29f3f5364fcbf2d2c5c1b
Yuniel Acosta Pérez <yunielacosta738@gmail.com>
2023-02-22 14:52:24 -0300

    feat(module): add data module
    change ULID converter 🦾

--- libs/data/src/main/kotlin/com/astrum/data/ModifiableULIDEntity.kt
+++ libs/data/src/main/kotlin/com/astrum/data/ModifiableULIDEntity.kt
@@ -5,8 +5,8 @@ import java.time.Instant
 
 abstract class ModifiableULIDEntity : ULIDEntity(), Modifiable {
     @GeneratedValue
-    override var createdAt: Instant? = Instant.now()
+    override var createdAt: Instant? = null
 
     @GeneratedValue
-    override var updatedAt: Instant? = Instant.now()
+    override var updatedAt: Instant? = null
 }
